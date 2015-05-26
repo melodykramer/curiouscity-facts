f6f9b73daca88da1a82a9c11486ad8a4c8234b25
chagan <chrishagan23@gmail.com>
2015-05-26 15:37:42 -0500

    Tweaks to fact tease and donate count, removing debugging console.logs

--- app/views.py
+++ app/views.py
@@ -23,17 +23,11 @@ def before_request():
 @app.route('/')
 def index():
 	randFact = get_first()
-	# return render_template('index.html', data=randFact)
 	return redirect(url_for('posts',id=randFact['ID']))
-	# return jsonify(randFact)
 
 @app.route('/random')
 def random_fact():
 	randFact = random.choice(g.data)
-	#return render_template('index.html', data=randFact)
-	# return redirect(url_for('posts',id=randFact['id']))
-	# return jsonify(randFact)
-	# return render_template(randFact['Template']+'.html',data=randFact)
 	return randFact['ID']
 
 @app.route('/api/random')
@@ -73,7 +67,7 @@ def page_not_found(e):
 def mailto(fact,ID):
 	url = 'http://curiousfacts.wbez.org/'+ID
 	url_parse = quote(url)
-	body = """Hi!\n\nI was just flipping through Curious City's Fact Generator and learned this interesting thing about Chicago:\n\n%s\n\nThey've got more where that came from:\n\n%s""" % (fact,url)
+	body = """Hi!\n\nI was just flipping through Curious City's Fact Generator and learned this interesting thing about Chicago:\n\n%s\n\nThey've got more where that came from:\n\n%s""" % (fact,url_parse)
 	subject_parse = quote('Found a neat fact about Chicago for you')
 	body_parse = quote(body)
 
